7fb8a681bec6d4447687d9b57da69f04b1885033
adityadewhy-local <adityadewhy@gmail.com>
2025-12-03 20:57:10 +0530

    rotator extension

---
+++ rotate-video-extension/background.js
@@ -0,0 +1,29 @@
+
+chrome.action.onClicked.addListener((tab) => {
+  rotate(tab);
+});
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "rotate_videos") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      rotate(tabs[0]);
+    });
+  }
+});
+
+function rotate(tab) {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: rotateVideos
+  });
+}
+
+function rotateVideos() {
+  document.querySelectorAll("video").forEach(v => {
+    let angle = parseInt(v.dataset.angle || "0", 10);
+    angle = (angle + 90) % 360;
+    v.dataset.angle = angle;
+    v.style.transform = `rotate(${angle}deg)`;
+    v.style.transformOrigin = "center center";
+  });
+}
